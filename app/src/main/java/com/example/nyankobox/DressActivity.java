52daedfb13acb9d8045cac203487d0fa501a0997
s3a2 <honami.e310@gmai.com>
2019-11-18 09:59:49 +0900

    11/15_egu

--- app/src/main/java/com/example/nyankobox/DressActivity.java
+++ app/src/main/java/com/example/nyankobox/DressActivity.java
@@ -2,7 +2,10 @@ package com.example.nyankobox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DressActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class DressActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dress);
+
+        //ホーム画面に遷移
+        ImageButton homesend = findViewById(R.id.homeBtn);
+        homesend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //日記記入画面に遷移
+        ImageButton diarysend = findViewById(R.id.diaryBtn);
+        diarysend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), DiaryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //ログ画面に遷移
+        ImageButton logsend = findViewById(R.id.logBtn);
+        logsend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), LogActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/nyankobox/LogActivity.java
+++ app/src/main/java/com/example/nyankobox/LogActivity.java
@@ -2,7 +2,10 @@ package com.example.nyankobox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class LogActivity extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class LogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+
+        //ホーム画面に遷移
+        ImageButton homesend = findViewById(R.id.homeBtn);
+        homesend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //日記記入画面に遷移
+        ImageButton diarysend = findViewById(R.id.diaryBtn);
+        diarysend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), DiaryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //着せ替え画面に遷移
+        ImageButton dresssend = findViewById(R.id.dressBtn);
+        dresssend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), DressActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
