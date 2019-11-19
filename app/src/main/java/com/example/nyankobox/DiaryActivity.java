b884e0492825a83d268837794cefb46f2dafaeae
s3a2 <cha2.05sugar@gmail.com>
2019-11-19 12:23:19 +0900

    11/19_zono 日記記入デザイン・SQLite

--- app/src/main/java/com/example/nyankobox/DiaryActivity.java
+++ app/src/main/java/com/example/nyankobox/DiaryActivity.java
@@ -3,8 +3,10 @@ package com.example.nyankobox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -56,6 +58,46 @@ public class DiaryActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), DressActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        /**
+         * 送信ボタン処理
+         */
+        ImageButton registerButton = findViewById(R.id.sendBtn);
+        // MemoOpenHelperクラスを定義
+        final dbData helper = null;
+        //初期値
+        String diaryStr = null;
+        final String emo = null;
+
+
+        // clickイベント追加
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //日付の取得
+                SimpleDateFormat dDate = new SimpleDateFormat("YYYY/MM/dd ");
+
+                // 入力内容を取得する
+                EditText diary = findViewById(R.id.editText);
+                String diaryStr = diary.getText().toString();
+
+                // DBに保存
+                SQLiteDatabase db = helper.getWritableDatabase();
+                try {
+                        // 新規作成の場合
+
+                        // INSERT
+                        db.execSQL("insert into NYANKO_TABLE(date,diary,emo) VALUES('"+ dDate +"','"+ diaryStr +"','"+ emo +"')");
+
+                } finally {
+                    // finallyは、tryの中で例外が発生した時でも必ず実行される
+                    // dbを開いたら確実にclose
+                    db.close();
+                }
+
             }
         });
 
