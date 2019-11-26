42439f9e57d2d667eca0b0a87804cc74a6ab4831
s3a2 <honami.e310@gmai.com>
2019-11-26 15:14:04 +0900

    11/26_egu プロフィール画面　完 パスコード画面なう

--- app/src/main/java/com/example/nyankobox/PrivacyActivity.java
+++ app/src/main/java/com/example/nyankobox/PrivacyActivity.java
@@ -2,9 +2,13 @@ package com.example.nyankobox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
 
 public class PrivacyActivity extends AppCompatActivity {
 
@@ -12,6 +16,32 @@ public class PrivacyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy);
+        CompoundButton sw;
+        final Button passChangeBtn;
+        final View line4;
+
+        //スイッチのインスタンス生成
+        sw = (CompoundButton) findViewById(R.id.passSw);
+        passChangeBtn = (Button) findViewById(R.id.passChangeBtn);
+        line4 =  (View) findViewById(R.id.line4);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked == true){
+                    //switchがONのとき
+                    passChangeBtn.setVisibility(View.VISIBLE);
+                    line4.setVisibility(View.VISIBLE);
+                    //パスコード設定画面表示
+                    Intent intent = new Intent(getApplication(), PassLockActivity.class);
+                    startActivity(intent);
+
+                }else if(isChecked == false){
+                    //switchがOFFのとき
+                    passChangeBtn.setVisibility(View.INVISIBLE);
+                    line4.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
         //戻るボタン押下で前画面に戻る
         ImageButton backBtn  = findViewById(R.id.backBtn);
--- app/src/main/java/com/example/nyankobox/ProfileActivity.java
+++ app/src/main/java/com/example/nyankobox/ProfileActivity.java
@@ -2,20 +2,27 @@ package com.example.nyankobox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class ProfileActivity extends AppCompatActivity {
     EditText nameText;
+    EditText birthdayDate;
     androidx.constraintlayout.widget.ConstraintLayout mainLayout;
     InputMethodManager inputMethodManager;
+
 
 
     @Override
@@ -39,6 +46,33 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        //誕生日選択
+        birthdayDate = (EditText) findViewById(R.id.birthdayDate);
+        birthdayDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                //Calendarインスタンスを取得
+                final Calendar date = Calendar.getInstance();
+                //DataPickerDialogインスタンスを取得
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        ProfileActivity.this, android.R.style.Theme_Holo_Dialog, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        birthdayDate.setText(String.format("%d 年 %02d 月 %02d 日", year, month+1, dayOfMonth));
+                    }
+                },
+                        date.get(Calendar.YEAR),
+                        date.get(Calendar.MONTH),
+                        date.get(Calendar.DATE)
+                );
+                //dialogを表示
+                datePickerDialog.show();
+            }
+        });
+
+
+
 
         //戻るボタン押下で前画面に戻る
         ImageButton backBtn  = findViewById(R.id.backBtn);
@@ -50,5 +84,15 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
-
+    /**
+     * EditText編集時に背景をタップしたらキーボードを閉じるようにするタッチイベントの処理
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //キーボードを隠す
+        inputMethodManager.hideSoftInputFromWindow(mainLayout.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        //背景にフォーカスを移す
+        mainLayout.requestFocus();
+        return false;
+    }
 }
