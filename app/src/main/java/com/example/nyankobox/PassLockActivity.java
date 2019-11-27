3d39ac9c3bd3cbc17c88b69fac9e0a7195d5ef08
s3a2 <honami.e310@gmai.com>
2019-11-27 09:45:22 +0900

    11/27_egu プロフィール画面　完 パスコード画面なう 2

---
+++ app/src/main/java/com/example/nyankobox/PassLockActivity.java
@@ -0,0 +1,22 @@
+package com.example.nyankobox;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+import java.util.HashSet;
+
+public class PassLockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass_lock);
+    }
+
+
+}
