43a3d5f16cc163ba018322a5e828e4ad37727dd2
s3a2 <honami.e310@gmai.com>
2019-12-10 15:24:31 +0900

    12/10_egu 使い方追記

--- app/src/main/java/com/example/nyankobox/HowtoActivity.java
+++ app/src/main/java/com/example/nyankobox/HowtoActivity.java
@@ -3,6 +3,9 @@ package com.example.nyankobox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class HowtoActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class HowtoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_howto);
+
+        //最前面に持ってくる
+        TextView textView2 = (TextView)findViewById(R.id.textView2);
+        View view = (View)findViewById(R.id.view);
+        textView2.bringToFront();
+        view.bringToFront();
+
+        TextView textView3 = (TextView)findViewById(R.id.textView3);
+        View view2 = (View)findViewById(R.id.view);
+        textView3.bringToFront();
+        view2.bringToFront();
+
+        //カスタムフォント
+
+
+        //戻るボタン押下で前画面に戻る
+        ImageButton backsend  = findViewById(R.id.backBtn);
+        backsend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
